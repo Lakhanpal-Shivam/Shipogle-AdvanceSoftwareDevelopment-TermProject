25f9719e46a630563d7edf5ceb97d59ddf709fb4
slakhanpal <sh475218@dal.ca>
2023-09-27 04:33:02 -0300

    Removed unwanted changes

--- shipogle-frontend/src/pages/RegistrationForm.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CommonRegForms from "../components/CommonRegForms";
-export default function RegistrationSender() {
-  return (
-    <div className="regPageForm">
-      <CommonRegForms />
-    </div>
-  );
-}
